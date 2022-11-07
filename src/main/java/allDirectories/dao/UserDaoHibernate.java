7e396e604e197633ad2db3a5dc577a4fda51f467
Schurov Konstantin <shurovkp@gmail.com>
2022-11-07 16:14:41 +0300

    readOnly,Repository,Constructor

--- src/main/java/allDirectories/dao/UserDaoHibernate.java
+++ src/main/java/allDirectories/dao/UserDaoHibernate.java
@@ -1,14 +1,13 @@
 package allDirectories.dao;
 
 import allDirectories.models.User;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 
-@Component
+@Repository
 public class UserDaoHibernate implements UserDao {
 
     @PersistenceContext
@@ -19,28 +18,29 @@ public class UserDaoHibernate implements UserDao {
         this.entityManager = entityManager;
     }
 
-    @Transactional
+
     public List<User> show() {
         return entityManager.createQuery("SELECT p from User p", User.class).getResultList();
     }
-    @Transactional
+
     public void delete(int id) {
         User user = entityManager.find(User.class, id);
         entityManager.remove(user);
     }
 
-    @Transactional
+
     public void add(User user) {
         entityManager.persist(user);
     }
-    @Transactional
+
+
     public void update(int id, User user) {
         User updatedUser = entityManager.find(User.class, id);
         updatedUser.setName(user.getName());
         updatedUser.setAge(user.getAge());
 
     }
-    @Transactional
+
     public User get(int id) {
         return  entityManager.find(User.class, id);
 
--- src/main/java/allDirectories/service/UserServiceHibernate.java
+++ src/main/java/allDirectories/service/UserServiceHibernate.java
@@ -4,36 +4,46 @@ import allDirectories.dao.UserDao;
 import allDirectories.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 public class UserServiceHibernate implements UserService {
 
+
+    private final UserDao userDao;
+
     @Autowired
-    UserDao userDao;
+    public UserServiceHibernate(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
-
+    @Transactional(readOnly = true)
     @Override
     public List<User> show() {
       return userDao.show();
     }
 
+    @Transactional
     @Override
     public void delete(int id) {
         userDao.delete(id);
     }
 
+    @Transactional
     @Override
     public void add(User user) {
         userDao.add(user);
     }
 
+    @Transactional
     @Override
     public void update(int id, User user) {
         userDao.update(id, user);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public User get(int id) {
        return userDao.get(id);
